6659294dfe3804515b914730e62c5d40b16c36cc
andersoncespedes <teanbronylatino@gmail.com>
2022-05-04 21:18:38 -0430

    progresion de pelea

--- js/sistem.js
+++ js/sistem.js
@@ -1,9 +1,79 @@
 
-    var sound = new Howl({
-      src: ['../audio/Final Fantasy IV.mp3'],
-      volume: 1.0,
-      onend: function () {
-        alert('We finished with the setup!');
-      }
-    });
-    sound.play()
+	const preguntas = [{
+		pregunta1:"¿Año del discurso 'i have a dream' de Martin Luther King?",
+		respuestas:['1967',
+			'1965',
+			'1963',
+			'1969',
+			 '1963']
+		
+	},{
+		pregunta1:'¿Apolo de la mitologia Griega era el dios de?',
+		respuestas:['Los Ladrones',
+			'La Humanidad',
+			'El Sol',
+			'LA Guerra',
+			 'El Sol']
+		
+	},{
+		pregunta1:'¿Diosa de la Luna segun la mitologia Griega?',
+		respuestas:['Artemisa',
+			'Apolo',
+			'Hermes',
+			'Zeus',
+			 'Artemisa']
+		
+	},{
+		pregunta1:'¿Quien fue el creador de los humanos segun la mitologia Griega?',
+		respuestas:['Zeus',
+			'Prometeo',
+			'Poseidon',
+			'Urano',
+			 'Prometeo']
+		
+	},{
+		pregunta1:'¿Hermano de Freyia de la mitologia nordica?',
+		respuestas:['Frey',
+			'Fir',
+			'Faldr',
+			'Loki',
+			 'Frey']
+		
+	},{
+		pregunta1:'¿Pokemones Iniciales de Pokemon Rojo y Azul?',
+		respuestas:['Bulbasaur‎, Charmander‎ y Squirtle‎',
+			'Treecko, Torchic y Mudkip',
+			'Chikorita,Cyndaquil y Totodile',
+			'Turtwig, Chimchar y Piplup',
+			 'Bulbasaur‎, Charmander‎ y Squirtle‎']
+		
+	},{
+		pregunta1:'¿QUIEN ES EL MAS SEXY?',
+		respuestas:['Anderson Cespexcvcdes',
+			'Jhoan Riverxcvxo',
+			'Christian Rivezxcxzro',
+			'Emilio Cespedzxces',
+			 'Anderson Ceszxcxzpedes']
+		
+	},{
+		pregunta1:'¿QUIEN ES EL MAS SEXY?',
+		respuestas:['Anderson Cespedes',
+			'Jhoan Rivero',
+			'Christian Rivero',
+			'Emilio Cespedes',
+			 'Anderson Cespedes']
+		
+	},
+	{
+		pregunta1:'¿QUIEN ES menos sexy?',
+		respuestas:[
+			'Anderds',
+		'Ja',
+			'Chd',
+			'Emds',
+			 'Anderds']
+		
+
+		}
+	
+	];
